26e6a5c1d1726fd7c37e65961dd42084a3a4b1ab
Prajwal Pai <prajwalpai21@gmail.com>
2025-11-29 01:19:01 +0530

    Backend feature ofr Signup, Users can now sign up to TempShare

--- TempShare/tempshare-frontend/src/pages/SignUpPage.jsx
+++ TempShare/tempshare-frontend/src/pages/SignUpPage.jsx
@@ -1,18 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignUpPage = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // data to send to the backend
+    const dataToSend = {
+      fullName: formData.fullName,
+      email: formData.email,
+      password: formData.password,
+    };
+    try {
+      const response = await fetch("http://localhost:8080/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataToSend),
+      });
+
+      const result = await response.text(); //backend returns plain text
+      setMessage(result);
+
+      if (result === "You have successfully sign up!") {
+        setFormData({
+          fullName: "",
+          email: "",
+          password: "",
+        });
+      }
+    } catch (error) {
+      setMessage("Something went wrong. Try again ");
+    }
+  };
   return (
-    
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="bg-white p-8 rounded-2xl shadow-xl w-full max-w-md">
         <h1 className="text-3xl fond-bold text-center mb-6">Create Account</h1>
-        <form className="space-y-4">
+
+        {/* a backend messgae */}
+        {message && (
+          <p className="text-center text-red-600 font-semibold mb-3">
+            {message}
+          </p>
+        )}
+
+        <form className="space-y-4" onSubmit={handleSubmit}>
           <div>
             <label className="block mb-1 font-medium">Full Name</label>
             <input
               type="text"
               className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Enter your name"
+              value={formData.fullName}
+              onChange={(e) =>
+                setFormData({ ...formData, fullName: e.target.value })
+              }
+              required
             />
           </div>
 
@@ -22,15 +73,25 @@ const SignUpPage = () => {
               type="text"
               className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Enter your email"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+              required
             />
           </div>
 
           <div>
             <label className="block mb-1 font-medium">Password</label>
             <input
-              type="text"
+              type="password"
               className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Create a password"
+              value={formData.password}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+              required
             />
           </div>
 
@@ -41,6 +102,7 @@ const SignUpPage = () => {
             Sign Up
           </button>
         </form>
+
         <p className="text-center text-sm mt-4">
           Already have an account?{" "}
           <a href="/" className="text-blue-600 underline">
