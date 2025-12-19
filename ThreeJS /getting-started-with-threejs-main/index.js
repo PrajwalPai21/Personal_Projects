da6ada8583a200805cbe5e7a0a4b5383263df5cb
Prajwal Pai <prajwalpai21@gmail.com>
2025-12-19 14:55:01 +0530

    New Geometry practice

--- ThreeJS /getting-started-with-threejs-main/index.js
+++ ThreeJS /getting-started-with-threejs-main/index.js
@@ -15,7 +15,7 @@ const aspect = w / h;
 const near = 0.1;
 const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+camera.position.z = 5;
 
 const scene = new THREE.Scene();
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -23,23 +23,35 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.01;
 
 // 2 = increse the mesh squares or somethin
-const shape = new THREE.IcosahedronGeometry(1.0, 3);
+const shape1 = new THREE.IcosahedronGeometry(1.0, 3);
+const shape2 = new THREE.BoxGeometry(1.0, 1);
+
 //MeshBasicMaterial doesnt interact with lights so use standard materoal
 const mat = new THREE.MeshStandardMaterial({
   color: 0xffffff,
   flatShading: true,
 });
 
-const mesh = new THREE.Mesh(shape, mat);
+const mesh = new THREE.Mesh(shape1, mat);
 scene.add(mesh);
+
+const mesh2 = new THREE.Mesh(shape2, mat);
+mesh2.position.set(-2, 1, -1);
+scene.add(mesh2);
+
 const wireMat = new THREE.MeshBasicMaterial({
   color: 0xffffff,
   wireframe: true,
 });
 
-const wireMesh = new THREE.Mesh(shape, wireMat);
+const wireMesh = new THREE.Mesh(shape1, wireMat);
 wireMesh.scale.setScalar(1.001);
 mesh.add(wireMesh); // doing scene.add would not make the mesh move when animated
+//hence make the wiremesh child of the mesh
+
+const wireMesh2 = new THREE.Mesh(shape2, wireMat);
+wireMesh2.scale.setScalar(1.001);
+mesh2.add(wireMesh2); // doing scene.add would not make the mesh move when animated
 //hence make the wiremesh child of the mesh
 
 // liighting top and bottom
@@ -53,5 +65,6 @@ function animate(t = 0) {
   mesh.rotation.x = t * 0.0001;
   renderer.render(scene, camera);
   controls.update(); // this is for a smooth movement when you hold and let go
+
 }
 animate();
